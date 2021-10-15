ea63dac0a8f39813c59b19531db12b6070eeb45c
AbdullaAlwaki <Abdullaalwaki@icloud.com>
2021-10-15 18:39:22 +0200

    add hour

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const daysEl = document.getElementById ('days')
 const hoursEl = document.getElementById('hours')
 const minsEl = document.getElementById('mins')
-const secondsEl = document.getElementById('seconds')
+const secondsEl = document.getElementById('seconds1')
 
 const newYears = "2 Apr 2022";
 
@@ -11,18 +11,64 @@ function countdown() {
 
     const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-    const days = Math.floor(totalSeconds / 3600 / 24)
+   // const days =
     const hours = Math.floor(totalSeconds / 3600) % 24;
     const mins = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds) % 60;
 
-    daysEl.innerHTML = days;
+    daysEl.innerHTML =  Math.floor(totalSeconds / 3600 / 24);
     hoursEl.innerHTML = hours;
     minsEl.innerHTML = mins;
     secondsEl.innerHTML = seconds;
 
 }
 
-setInterval(countdown, 1000);
+
+
+function updateClock() {
+    var now = new Date();
+    var dname = now.getDay(),
+        mo = now.getMonth(),
+        dnum = now.getDate(),
+        yr = now.getFullYear(),
+        hou = now.getHours(),
+        min = now.getMinutes(),
+        sec = now.getSeconds(),
+        pe = "PM";
+    
+    if (hou == 0) {
+        hou = 12;
+    }
+    if (hou > 12) {
+        hou = hou - 12;
+        pe = "AM";
+    }
+
+    Number.prototype.pad = function (digits){
+        for (var n = this.toString(); n.length < digits; n = 0 + n);
+        return n;
+    }
+    
+    var months = ["January", "February", "March", "April", "May", "June", "July", "Augest", "September", "October", "November", "December"];
+    var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var ids = ["dayname", "month", "daynum", "year", "hour", "minutes", "seconds", "period"];
+    var values = [week[dname], months[mo], dnum.pad(2), yr, hou.pad(2), min.pad(2), sec.pad(2), pe];
+    for (var i = 0; i < ids.length; i++) {
+        document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+        console.log(document.getElementById(ids[i]).firstChild.nodeValue = values[i]);
+    }
+        
+}
+
+setInterval(() => {
+
+    countdown();
+  
+    updateClock();
+  
+  }, 1000);
+  
+  
+
 
 
